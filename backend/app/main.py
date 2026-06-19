cfae202e2446dcb68a0c8570362c90b305df898f
Guy Abuav <guyabuav10@gmail.com>
2026-06-19 19:23:59 +0300

    fix cors whitelist

--- backend/app/main.py
+++ backend/app/main.py
@@ -55,7 +55,7 @@ app = FastAPI(title="Drone News API", version="0.1.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173", "http://127.0.0.1:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
